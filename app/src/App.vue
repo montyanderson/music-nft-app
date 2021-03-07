c46913e854528eb4d458e6bb2d91107fb1bd90f2
Monty Anderson <work@montyanderson.net>
2021-03-07 00:39:56 +0000

    display releases

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,11 +5,21 @@
 </template>
 
 <style>
+* {
+	margin: 0;
+	padding: 0;
+}
+
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: "Roboto", Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
+	color: white;
+
+	background: linear-gradient(180deg, #2F2D2D 0%, #121212 100%);
+
+	width: 100%;
+
 }
 </style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,7 +1,45 @@
+<style scoped>
+h1 {
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 70px;
+line-height: 82px;
+/* identical to box height */
+
+
+color: #FFFFFF;
+}
+
+.releases {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 10px;
+}
+
+.release {
+  grid-row: 1 / 3;
+}
+
+.release img {
+  width: 100%;
+  padding-top: 100%;
+}
+
+p {
+  font-size: 2rem;
+}
+</style>
+
 <template>
   <div class="home">
-    <div v-for="release in releases">
-      <img v-bind:src="release.imageUrl">
+    <h1>Releases</h1>
+
+    <div class="releases" >
+      <div class="release" v-for="release in releases">
+        <img v-bind:src="release.imageUrl">
+        <p>{{release.name}}</p>
+      </div>
     </div>
   </div>
 </template>
