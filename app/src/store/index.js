2771420c9d04f3a927ae7eeef7d60fb03b389d6d
Monty Anderson <work@montyanderson.net>
2021-03-09 01:32:19 +0000

    setup prettier

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,172 +1,170 @@
-import url from 'url';
-import Vue from 'vue';
-import Vuex from 'vuex';
-import Contract from 'web3-eth-contract';
-import Web3WsProvider from 'web3-providers-ws';
-import S3 from 'aws-sdk/clients/s3';
-import abi from '../abi.json';
-
+import url from "url";
+import Vue from "vue";
+import Vuex from "vuex";
+import Contract from "web3-eth-contract";
+import Web3WsProvider from "web3-providers-ws";
+import S3 from "aws-sdk/clients/s3";
+import abi from "../abi.json";
 
 // Contract.setProvider(web3.currentProvider);
-Contract.setProvider(new Web3WsProvider("wss://ropsten.infura.io/ws/v3/e0521fe0263543b79880ef251466bf33"));
+Contract.setProvider(
+	new Web3WsProvider(
+		"wss://ropsten.infura.io/ws/v3/e0521fe0263543b79880ef251466bf33"
+	)
+);
 
-const ocean = window.ocean = new Contract(abi, "0x80A0f2482c5BcB72fF39835Dd2EE90ADc0352946");
+const ocean = (window.ocean = new Contract(
+	abi,
+	"0x80A0f2482c5BcB72fF39835Dd2EE90ADc0352946"
+));
 
 Vue.use(Vuex);
 
 async function getCredentialsFromAccess(access_grant) {
-    const storedCredentials = localStorage.getItem(`access:${access_grant}`);
+	const storedCredentials = localStorage.getItem(`access:${access_grant}`);
 
-    if(typeof storedCredentials === "string") {
-        console.log("returning credentials from cache");
-        return JSON.parse(storedCredentials);
-    }
+	if (typeof storedCredentials === "string") {
+		console.log("returning credentials from cache");
+		return JSON.parse(storedCredentials);
+	}
 
-    const response = await fetch("https://auth.tardigradeshare.io/v1/access", {
-        method: "POST",
-        body: JSON.stringify({
-            access_grant,
-            public: false
-        })
-    });
+	const response = await fetch("https://auth.tardigradeshare.io/v1/access", {
+		method: "POST",
+		body: JSON.stringify({
+			access_grant,
+			public: false
+		})
+	});
 
-    const {
-        access_key_id,
-        secret_key
-    } = await response.json();
+	const { access_key_id, secret_key } = await response.json();
 
-    const credentials = {
-        accessKey: access_key_id,
-        secretKey: secret_key
-    };
+	const credentials = {
+		accessKey: access_key_id,
+		secretKey: secret_key
+	};
 
-    localStorage.setItem(`access:${access_grant}`, JSON.stringify(credentials));
+	localStorage.setItem(`access:${access_grant}`, JSON.stringify(credentials));
 
-    return credentials;
-};
+	return credentials;
+}
 
 const urlCache = new Map();
 
 async function getUrlFromStorjUri(uri) {
-    if(urlCache.has(uri) === true) {
-        return urlCache.get(uri);
-    }
+	if (urlCache.has(uri) === true) {
+		return urlCache.get(uri);
+	}
 
-    const {auth, host, path} = url.parse(uri);
-    console.log({auth, host, path});
+	const { auth, host, path } = url.parse(uri);
+	console.log({ auth, host, path });
 
-    const {accessKey, secretKey} = await getCredentialsFromAccess(auth);
-    console.log({accessKey, secretKey});
+	const { accessKey, secretKey } = await getCredentialsFromAccess(auth);
+	console.log({ accessKey, secretKey });
 
-    const s3 = new S3({
-        accessKeyId: accessKey,
-        secretAccessKey: secretKey,
-        endpoint: "https://gateway.tardigradeshare.io",
-        s3ForcePathStyle: true,
-        signatureVersion: "v4"
-    });
+	const s3 = new S3({
+		accessKeyId: accessKey,
+		secretAccessKey: secretKey,
+		endpoint: "https://gateway.tardigradeshare.io",
+		s3ForcePathStyle: true,
+		signatureVersion: "v4"
+	});
 
-    const signedUrl = s3.getSignedUrl('getObject', {
-        Bucket: host,
-        Key: path.slice(1)
-    });
+	const signedUrl = s3.getSignedUrl("getObject", {
+		Bucket: host,
+		Key: path.slice(1)
+	});
 
-    urlCache.set(uri, signedUrl);
-    
-    return signedUrl;
+	urlCache.set(uri, signedUrl);
+
+	return signedUrl;
 }
 
 async function getRelease(releaseId) {
-    const tokenId = Number(await ocean.methods.getTokenByRelease(releaseId, 0).call());
-    console.log({ releaseId, tokenId });
+	const tokenId = Number(
+		await ocean.methods.getTokenByRelease(releaseId, 0).call()
+	);
+	console.log({ releaseId, tokenId });
 
-    
+	// e.g. sj://myaccess:mybucket/metadata.json
+	const metadataUri = await ocean.methods.tokenURI(tokenId).call();
+	console.log({ metadataUri });
 
-    // e.g. sj://myaccess:mybucket/metadata.json
-    const metadataUri = await ocean.methods.tokenURI(tokenId).call();
-    console.log({metadataUri});
+	let available = 0;
 
-    let available = 0;
+	// e.g. https://gateway.tardigradeshare.io/...
+	const metadataUrl = await getUrlFromStorjUri(metadataUri);
+	console.log({ metadataUrl });
 
-   
+	// NFT metadata format
+	const metadata = await (await fetch(metadataUrl)).json();
+	console.log({ metadata });
 
-    // e.g. https://gateway.tardigradeshare.io/...
-    const metadataUrl = await getUrlFromStorjUri(metadataUri);
-    console.log({metadataUrl});
+	// images are typically relative to metadata.json
+	const imageUri = url.resolve(metadataUri, metadata.image);
+	console.log({ imageUri });
 
-    // NFT metadata format
-    const metadata = await (await fetch(metadataUrl)).json();
-    console.log({metadata});
+	const imageUrl = await getUrlFromStorjUri(imageUri);
+	console.log({ imageUrl });
 
-    // images are typically relative to metadata.json
-    const imageUri = url.resolve(metadataUri, metadata.image);
-    console.log({imageUri});
+	const release = {
+		id: releaseId,
+		name: metadata.name,
+		artist: metadata.artist,
+		tracks: metadata.tracks,
+		imageUrl: imageUrl
+	};
 
-    const imageUrl = await getUrlFromStorjUri(imageUri);
-    console.log({imageUrl});
+	// wait for image to load into browser cache
+	await new Promise(resolve => {
+		const preload = new Image();
 
-    const release = {
-        id: releaseId,
-        name: metadata.name,
-        artist: metadata.artist,
-        tracks: metadata.tracks,
-        imageUrl: imageUrl
-    };
+		preload.onload = resolve;
+		preload.src = release.imageUrl;
+	});
 
-    // wait for image to load into browser cache
-    await new Promise(resolve => {
-        const preload = new Image();
-
-        preload.onload = resolve;
-        preload.src = release.imageUrl;
-    });
-
-    return release;
+	return release;
 }
 
 export default new Vuex.Store({
 	state: {
 		releases: [],
-        releasesLoading: true
+		releasesLoading: true
 	},
 	mutations: {
 		pushRelease(state, release) {
-			const releases = [
-                ...state.releases,
-                release
-            ];
+			const releases = [...state.releases, release];
 
-            state.releases = releases;
+			state.releases = releases;
 		},
 
-        finishLoading(state) {
-            state.releasesLoading = false;
-        }
-	},
-	actions: {
-		async getReleases({commit, state}) {
-            await new Promise(r => setTimeout(r, 200));
-
-            console.log(ocean);
-
-            let totalReleases = Number(await ocean.methods.getReleaseSupply().call());
-
-            const releaseIds = [];
-
-            for(let i = 0; i < totalReleases; i++) {
-                releaseIds.push(i);
-            }
-
-            const releases = await Promise.all(releaseIds.map(getRelease));
-
-            for(const release of releases) {
-                commit("pushRelease", release);
-            }
-
-            commit('finishLoading');
+		finishLoading(state) {
+			state.releasesLoading = false;
 		}
 	},
-	modules: {
-	}
-});
+	actions: {
+		async getReleases({ commit, state }) {
+			await new Promise(r => setTimeout(r, 200));
 
+			console.log(ocean);
+
+			let totalReleases = Number(
+				await ocean.methods.getReleaseSupply().call()
+			);
+
+			const releaseIds = [];
+
+			for (let i = 0; i < totalReleases; i++) {
+				releaseIds.push(i);
+			}
+
+			const releases = await Promise.all(releaseIds.map(getRelease));
+
+			for (const release of releases) {
+				commit("pushRelease", release);
+			}
+
+			commit("finishLoading");
+		}
+	},
+	modules: {}
+});
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,177 +1,171 @@
 <style scoped>
 .releaseText {
-  font-family: Inter;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 70px;
-  line-height: 82px;
-  /* identical to box height */
+	font-family: Inter;
+	font-style: normal;
+	font-weight: bold;
+	font-size: 70px;
+	line-height: 82px;
+	/* identical to box height */
 
+	color: #ffffff;
 
-  color: #FFFFFF;
-
-  margin-left: 5%;
-  margin-top: 100px;
+	margin-left: 5%;
+	margin-top: 100px;
 }
 
 .releases {
-  margin-top: 5rem;
+	margin-top: 5rem;
 
-  width: 90%;
-  margin-left: auto;
-  margin-right: auto;
-  display: flex;
-   flex-direction: row;
-   flex-grow: 2;
+	width: 90%;
+	margin-left: auto;
+	margin-right: auto;
+	display: flex;
+	flex-direction: row;
+	flex-grow: 2;
 
-   flex-wrap:  wrap ;
-  grid-gap: 50px;
+	flex-wrap: wrap;
+	grid-gap: 50px;
 }
 
 .release {
-  grid-row: 1 / 3;
-  cursor: pointer;
+	grid-row: 1 / 3;
+	cursor: pointer;
 }
 
 .release img {
-  width: 250px;
+	width: 250px;
 }
 
-.header{
-  position:absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 80px;
+.header {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 80px;
 }
 
-.connectBtn{
-  position: absolute;
-  background: white;
-  width: 180px;
-  height: 60px;
-  right: 10px;
-  top: 10px;
+.connectBtn {
+	position: absolute;
+	background: white;
+	width: 180px;
+	height: 60px;
+	right: 10px;
+	top: 10px;
 }
-.connectText{
-  position: absolute;
-  color: black;
-  font-size: 16px;
-  font-weight: 600;
-  font-style: normal;
-  top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
-white-space: nowrap;
+.connectText {
+	position: absolute;
+	color: black;
+	font-size: 16px;
+	font-weight: 600;
+	font-style: normal;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	white-space: nowrap;
 }
 
 .name {
-  font-size: 1.5rem;
-  text-align: left;
-  margin-top: 1rem;
+	font-size: 1.5rem;
+	text-align: left;
+	margin-top: 1rem;
 }
 
-@media only screen and (max-width: 1150px){
-   .releases {
- 
-
+@media only screen and (max-width: 1150px) {
+	.releases {
+	}
 }
-}
-@media only screen and (max-width: 620px){
-
-  .releases{
-    width: 95%;
-    margin-left: 10px;
-    grid-gap: 20px;
-  }
- .release img {
-  width: 45vw;
-}
-.releaseText{
-  font-size: 60px;
-  margin-left: 10px;
-}
+@media only screen and (max-width: 620px) {
+	.releases {
+		width: 95%;
+		margin-left: 10px;
+		grid-gap: 20px;
+	}
+	.release img {
+		width: 45vw;
+	}
+	.releaseText {
+		font-size: 60px;
+		margin-left: 10px;
+	}
 }
 
-@media only screen and (max-width: 500px){
-
-  .releases{
-     flex-direction: column;
-  flex-wrap:   nowrap;
-  margin-left: 20px;
-  margin-top: 100px;
-  }
-  .release img {
-  width: 90px;
- margin-top: -70px;
-}
-  .releaseText{
-  font-size: 50px;
-  margin-top: 80px;
-  margin-left: 20px;
-}
-.name {
-  position: relative;
-  top: -91px;
-  left: 110px;
-font-weight: 600;
-  font-size: 1.4rem;
-  text-align: left;
-  margin-top: 1rem;
-}
-.artist{
-  position: relative;
-  top: -87px;
-  left: 110px;
-}
-}
-
-@media only screen and (max-width: 300px){
- .releaseText{
-  font-size: 40px;
-}
+@media only screen and (max-width: 500px) {
+	.releases {
+		flex-direction: column;
+		flex-wrap: nowrap;
+		margin-left: 20px;
+		margin-top: 100px;
+	}
+	.release img {
+		width: 90px;
+		margin-top: -70px;
+	}
+	.releaseText {
+		font-size: 50px;
+		margin-top: 80px;
+		margin-left: 20px;
+	}
+	.name {
+		position: relative;
+		top: -91px;
+		left: 110px;
+		font-weight: 600;
+		font-size: 1.4rem;
+		text-align: left;
+		margin-top: 1rem;
+	}
+	.artist {
+		position: relative;
+		top: -87px;
+		left: 110px;
+	}
 }
 
-
-
-
-
+@media only screen and (max-width: 300px) {
+	.releaseText {
+		font-size: 40px;
+	}
+}
 </style>
 
 <template>
-  <div class="home">
-    <h1 class="releaseText">Releases</h1>
-    <div class="header">
-<div class="connectBtn">
-  <h2 class="connectText">Connect to a wallet</h2>
-</div>
-</div>
-    <div class="releases">
-      <div class="release" v-for="release in releases" v-on:click="goToRelease(release)">
-        <img v-bind:src="release.imageUrl">
-        <p class="name">{{release.name}}</p>
-        <p class="artist">{{release.artist}}</p>
-      </div>
-    </div>
-  </div>
+	<div class="home">
+		<h1 class="releaseText">Releases</h1>
+		<div class="header">
+			<div class="connectBtn">
+				<h2 class="connectText">Connect to a wallet</h2>
+			</div>
+		</div>
+		<div class="releases">
+			<div
+				class="release"
+				v-for="release in releases"
+				v-on:click="goToRelease(release)"
+			>
+				<img v-bind:src="release.imageUrl" />
+				<p class="name">{{ release.name }}</p>
+				<p class="artist">{{ release.artist }}</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  computed: {
-    releases() {
-      return this.$store.state.releases;
-    },
-    releasesLoading() {
-      return this.$store.state.releasesLoading;
-    }
-  },
-  methods: {
-      goToRelease({id}) {
-          this.$router.push({
-              path: `/release/${id}`
-          })
-      }
-  }
-}
+	computed: {
+		releases() {
+			return this.$store.state.releases;
+		},
+		releasesLoading() {
+			return this.$store.state.releasesLoading;
+		}
+	},
+	methods: {
+		goToRelease({ id }) {
+			this.$router.push({
+				path: `/release/${id}`
+			});
+		}
+	}
+};
 </script>
